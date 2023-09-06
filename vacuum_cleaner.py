6ae9cb73061e733868e21492b729ce3e054ceb1f
alexandre-zeni <alexandre.zeni@philips.com>
2023-09-06 10:16:59 -0300

    small changes

--- vacuum_cleaner.py
+++ vacuum_cleaner.py
@@ -13,35 +13,29 @@ plot_matrix = np.array(
  ]
 )
 
-vaccum_position_x = 1
-vaccum_position_y = 1
-
 def first_show_plot():
  initialize_plot_matrix_with_dirt()
  show_plot_with_positions()
 
 def walking_plots():
  global plot_matrix
- global vaccum_position_x
- global vaccum_position_y
+ global vacuum_position_x
+ global vacuum_position_y
 
  while is_plot_dirty():
-  vaccum_position_x = randint(1, 4)
-  vaccum_position_y = randint(1, 4)
+  vacuum_position_x = randint(1, 4)
+  vacuum_position_y = randint(1, 4)
   show_plot_with_positions()
 
-  if (plot_matrix[vaccum_position_x][vaccum_position_y] == 2):
-    plot_matrix[vaccum_position_x][vaccum_position_y] = 0
+  if (plot_matrix[vacuum_position_x][vacuum_position_y] == 2):
+    plot_matrix[vacuum_position_x][vacuum_position_y] = 0
  
  show_plot_with_positions()
 
 def show_plot_with_positions():
- global vaccum_position_x
- global vaccum_position_y
-
  plt.imshow(plot_matrix, 'gray')
  plt.nipy_spectral()
- plt.plot(vaccum_position_x, vaccum_position_y, marker='o', color='r', ls='')
+ plt.plot(vacuum_position_x, vacuum_position_y, marker='o', color='r', ls='')
  plt.show(block=False)
  plt.pause(0.5)
  plt.clf()
@@ -60,19 +54,22 @@ def get_dirt_number():
  return dirt_number
 
 def initialize_plot_matrix_with_dirt():
+ global plot_matrix
+
  for _ in range(get_dirt_number()):
   dirt_position_x = randint(1, 4)
   dirt_position_y = randint(1, 4)
   plot_matrix[dirt_position_x][dirt_position_y] = 2
 
 def is_plot_dirty():
- global plot_matrix
-
- for row_value in range(len(plot_matrix[vaccum_position_x])):
-  for column_value in range(len(plot_matrix[vaccum_position_y])):
+ for row_value in range(len(plot_matrix[vacuum_position_x])):
+  for column_value in range(len(plot_matrix[vacuum_position_y])):
    if (plot_matrix[row_value][column_value] == 2):
     return True
  return False
+
+vacuum_position_x = 1
+vacuum_position_y = 1
 
 first_show_plot()
 walking_plots()
